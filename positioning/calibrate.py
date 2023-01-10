c48ffa5728f2837ac5585b853b1a729f61080fda
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-01-10 12:58:25 -0500

    ammended code

--- positioning/calibrate.py
+++ positioning/calibrate.py
@@ -7,7 +7,7 @@ import glob
 import json
  
 # Defining the dimensions of checkerboard ##THE 30 IS FOR LENGTH OF SMALL SQUARE
-CHECKERBOARD = (6,9)
+CHECKERBOARD = (24,32)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
  
 # Creating vector to store vectors of 3D points for each checkerboard image
@@ -22,7 +22,8 @@ objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 prev_img_shape = None
  
 # Extracting path of individual image stored in a given directory
-images = glob.glob('./checkerboardpics/*.jpg')
+gray = 0
+images = glob.glob('positioning/checkerboardpics/*.jpg')
 for fname in images:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -35,22 +36,25 @@ for fname in images:
     we refine the pixel coordinates and display 
     them on the images of checker board
     """
-    if ret == True:
+    if ret:
         objpoints.append(objp)
         # refining pixel coordinates for given 2d points.
         corners2 = cv2.cornerSubPix(gray, corners, (11,11),(-1,-1), criteria)
          
         imgpoints.append(corners2)
+
+        print("FOUND CHESSBOARD")
  
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
-     
+    else:
+        print("NO CHESSBOARD")
+        
     cv2.imshow('img',img)
     cv2.waitKey(0)
  
 cv2.destroyAllWindows()
  
-h,w = img.shape[:2]
  
 """
 Performing camera calibration by 
