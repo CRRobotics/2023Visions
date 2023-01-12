4adfd77ad3efed0caf39741e8859908f40ac7d05
crr <crr@vision.lan>
2023-01-11 19:04:16 -0500

    got constants + vectors

--- positioning/apriltagtesting.py
+++ positioning/apriltagtesting.py
@@ -5,13 +5,13 @@ from functions import *
 import numpy as np
 print("We out")
 
-cap = cv.VideoCapture(1)
+cap = cv.VideoCapture(0)
 
 while True:
     success, frame = cap.read()
 
     #UNCOMMENT THIS IF YOU WANT TO TEST ON AN IMG
-    frame = cv.imread(r"ConePlacement\sample_images\Straight__Left_187in.png")
+    # frame = cv.imread(r"ConePlacement/sample_images/Straight__Left_187in.png")
 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
@@ -21,42 +21,45 @@ while True:
     if detections:
         for detection in detections:
 
-            if detection.tag_id not in [1,2,3,4,5,6,7,8] and len(detection.corners) != 4:
-                continue
+            if detection.tag_id in [1] and len(detection.corners) == 4:
+                
+                corner_counter = 1
+                for x, y in detection.corners:
+                    corner = (int(x), int(y))
+                    cv.putText(frame, f"{corner_counter}", corner, cv.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0))
+                    cv.circle(frame, corner, 5, (255,0,0), -1)
+                    corner_counter += 1
 
-            corner_counter = 1
-            for x, y in detection.corners:
-                corner = (int(x), int(y))
-                cv.putText(frame, f"{corner_counter}", corner, cv.FONT_HERSHEY_SIMPLEX, 1, (0,255, 0))
-                cv.circle(frame, corner, 5, (255,0,0), -1)
-                corner_counter += 1
+                cx, cy = detection.center
+                cv.circle(frame, (int(cx), int(cy)), 5, (0, 0, 255), -1)
+                cv.putText(frame, f"id: {detection.tag_id}", (int(cx), int(cy) + 20), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255, 0))
 
-            cx, cy = detection.center
-            cv.circle(frame, (int(cx), int(cy)), 5, (0, 0, 255), -1)
-            cv.putText(frame, f"id: {detection.tag_id}", (int(cx), int(cy) + 20), cv.FONT_HERSHEY_SIMPLEX, 1, (255,255, 0))
+                objectpoints = []
 
-            objectpoints = []
-
-            objectpoints.append((-0.0762, 0.0762, 0.0))
-            objectpoints.append((0.0762, 0.0762, 0.0))
-            objectpoints.append((0.0762, -0.0762, 0.0))
-            objectpoints.append((-0.0762, -0.0762, 0.0))
-
-            objectpoints = np.array(objectpoints)
+                for coord in constants.GET_CORNERS_MAT:
+                    print(detection.tag_id)
+                    objectpoints.append(coord + constants.ID_POS[detection.tag_id]["center"])
 
 
+                objectpoints = np.array(objectpoints)
 
-            mmat, tvec, rvec = cv.solvePnP(
-                objectpoints, 
-                detection.corners, 
-                constants.CAMERA_MATRIX,
-                constants.CAMERA_DIST, 
-                )
-            imgpts, jac = cv.projectPoints(objectpoints, rvec, tvec, constants.CAMERA_MATRIX, constants.CAMERA_DIST)
 
-            frame = draw(frame, detection.corners, imgpts)
 
-            print(tvec, rvec)
+                mmat, tvec, rvec = cv.solvePnP(
+                    objectpoints, 
+                    detection.corners, 
+                    constants.CAMERA_MATRIX,
+                    constants.CAMERA_DIST, 
+                    )
+                # imgpts, jac = cv.projectPoints(objectpoints, rvec, tvec, constants.CAMERA_MATRIX, constants.CAMERA_DIST)
+
+                # frame = draw(frame, detection.corners, imgpts)
+
+                print("TVECS:")
+                print(tvec)
+                print("RVECS:")
+                print(rvec)
+                print("NEW")
 
 
 
--- positioning/constants.py
+++ positioning/constants.py
@@ -19,8 +19,47 @@ DETECTOR = Detector(
 
 
 
-with open("positioning\cameraconstants.json") as f:
+with open("positioning/cameraconstants.json") as f:
     data = json.load(f)
     CAMERA_MATRIX = np.array(data["matrix"])
     CAMERA_DIST = np.array(data["distortion"])
 
+
+
+ID_POS = {
+    #Coordinates taken from field cad
+    1:{
+        # "center":(-7.243, 2.9365, 0.463),
+        "center":(0, 0, 0),
+    },
+    2:{
+        "center":(-7.243, 1.26, 0.463),   
+    },
+    3:{
+        "center":(-7.243, -0.416, 0.463),   
+    },
+    4:{
+        "center":(-7.908, -2.7415, 0.6955),   
+    },
+    5:{
+        "center":(7.908, -2.7415, 0.6955),   
+    },
+    6:{
+        "center":(7.243, -0.416, 0.463),   
+    },
+    7:{
+        "center":(7.243, 1.26, 0.463),   
+    },
+    8:{
+        "center":(7.243, 2.9365, 0.463),   
+    }
+
+}
+
+
+GET_CORNERS_MAT = np.array(
+    [[-0.0762, -0.0762, 0.0],
+    [0.0762, -0.0762, 0.0],
+    [0.0762, 0.0762, 0.0],
+    [-0.0762, 0.0762, 0.0]]
+)
