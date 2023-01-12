2cf53a51289b0b3516a564f6ee25c12144d490e2
crr <crr@vision.lan>
2023-01-11 19:38:02 -0500

    added modularization

--- positioning/apriltagtesting.py
+++ positioning/apriltagtesting.py
@@ -6,7 +6,7 @@ import numpy as np
 print("We out")
 
 cap = cv.VideoCapture(0)
-cap2 = cv.VideoCapture(1)
+cap2 = cv.VideoCapture(2)
 
 while True:
     success, frame1 = cap.read()
@@ -19,7 +19,12 @@ while True:
 
     vecsdict = getVecs(gray, constants.CAMERA_MATRIX1, constants.CAMERA_DIST1)
     print(vecsdict)
-    cv.imshow("IMAGE", gray)
+    gray2 = cv.cvtColor(frame2, cv.COLOR_BGR2GRAY)
 
+    vecsdict2 = getVecs(gray2, constants.CAMERA_MATRIX2, constants.CAMERA_DIST2)
+    print(vecsdict2)
+
+    cv.imshow("FRAME1", gray)
+    cv.imshow("FRAME2", gray2)
 
     cv.waitKey(1)
