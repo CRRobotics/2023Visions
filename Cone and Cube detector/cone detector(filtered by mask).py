e40ee6ad1952b5ce646e0b8d5cccf092811c7710
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-01-12 11:46:09 -0500

    put line detection BEFORE drawing hulls

--- Cone and Cube detector/cone detector(filtered by mask).py
+++ Cone and Cube detector/cone detector(filtered by mask).py
@@ -1,6 +1,6 @@
 import cv2  
 from functions import *
-vid = cv2.VideoCapture(0)
+vid = cv2.VideoCapture(r"Cone and Cube detector\TestVideo.mp4")
 
 
  
@@ -8,9 +8,9 @@ while True:
     
     success, img = vid.read()  
     #ret is a bool for whether frame is captured sussessfully
-    img = cv2.imread(r"positioning/sample_images/162_36_Angle.png")
+    # img = cv2.imread(r"positioning/sample_images/162_36_Angle.png")
     # Display the resulting frame
-    # After the loop release the cap object
+    # After the loop release the cap obj0ect
 
     mask=binarizeSubt(img)
     kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
@@ -20,25 +20,25 @@ while True:
     contours,hierarchy=cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
     retained = filtercontours(contours)
-    biggest = cv2.convexHull(max(retained, key=cv2.contourArea))
-    cv2.drawContours(img,[biggest],0,(255,0,255),3)
-    centers=getCenters(img,retained)
 
+    if retained:
+        biggest = cv2.convexHull(max(retained, key=cv2.contourArea))
+        centers=getCenters(img,retained)
+        lines:list[Line] = find_lines(img)
 
-    lines:list[Line] = find_lines(img)
+        for l in lines:
+            p = (l.x1, l.y1)
+            p2 = (l.x2, l.y2)
 
-    for l in lines:
-        p = (l.x1, l.y1)
-        p2 = (l.x2, l.y2)
+            if in_hull(biggest, p) and in_hull(biggest, p):
+                cv2.circle(img, p, 2, (255, 255, 255), 3)
+                cv2.circle(img, p2, 2, (255, 255, 255), 3)
 
-        if in_hull(biggest, p) and in_hull(biggest, p):
-            cv2.circle(img, p, 2, (255, 255, 255), 3)
-            cv2.circle(img, p2, 2, (255, 255, 255), 3)
+                cv2.line(img, p, p2, (255, 0, 0), 2)
+        cv2.drawContours(img,[biggest],0,(255,0,255),3)
+        cv2.imshow('s',img)
 
-            cv2.line(img, p, p2, (255, 0, 0), 2)
-    cv2.imshow('s',img)
-
-    cv2.waitKey(0)
+    cv2.waitKey(1)
 
 
 
