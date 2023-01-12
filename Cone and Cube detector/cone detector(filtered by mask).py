4df304767141cab6bfc7e8cbfe2d516bbc6ae007
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-01-11 22:06:38 -0500

    fixed some errors

--- Cone and Cube detector/cone detector(filtered by mask).py
+++ Cone and Cube detector/cone detector(filtered by mask).py
@@ -7,6 +7,7 @@ while(vid.isOpened()):
     
     ret, img = vid.read()  
     #ret is a bool for whether frame is captured sussessfully
+    img = cv2.imread(r"positioning/sample_images/162_36_Angle.png")
     # Display the resulting frame
     if ret == True:
         # After the loop release the cap object
@@ -16,11 +17,15 @@ while(vid.isOpened()):
             yellow=cv2.subtract(green,blue)
             ret,binImage = cv2.threshold(yellow, 40, 255, cv2.THRESH_BINARY)
             return binImage
+
         mask=binarizeSubt(img)
         kernel=cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
         mask=cv2.erode(mask,kernel,iterations=3)
         mask=cv2.dilate(mask,kernel,iterations=1)
+
         contours,hierarchy=cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+
         def getCenters(img, contours):
             """_, contours, _ = cv2.findContours
             returns centers of all polygons"""
@@ -36,18 +41,9 @@ while(vid.isOpened()):
             countourArea=cv2.contourArea(contours[i])
             ratio=countourArea/(w*h)
             
-            if ratio >=0.6 or ratio <= 0.4:
-            
-                continue
-            if countourArea <=100: # and contourArea > maxArea:
-               
-              
-            
-                continue
-            
-            cv2.drawContours(img,[contours[i]],0,(255,0,255),3)
-            #contours is a tuple of arrays
-        centers=getCenters(img,contours)
+            if not (ratio >=0.6 or ratio <= 0.4 or countourArea <= 100):
+                cv2.drawContours(img,[contours[i]],0,(255,0,255),3)
+                centers=getCenters(img,contours)
         cv2.imshow('s',img)
     	
     # the 'q' button is set for break
