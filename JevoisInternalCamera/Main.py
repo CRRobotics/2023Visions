431040874021823174217ddaac487355854abbc6
Rocky0Shao <122405175+Rocky0Shao@users.noreply.github.com>
2023-02-04 10:57:06 -0500

    jevois working version

--- JevoisInternalCamera/Main.py
+++ JevoisInternalCamera/Main.py
@@ -1,39 +1,29 @@
-import libjevois as jevois # type: ignore
+import libjevois as jevois
 import cv2
-from datetime import datetime
-import math
 import numpy as np
-import re
+import math
+
+## Tell Orientation of Cones
+#
+# Add some description of your module here.
+#
+# @author Rocky Shao
+# 
+# @videomapping YUYV 640 480 30 YUYV 640 480 30 Orientation Orientation
+# @email rocky.shao@icsd.k12.ny.us
+# @address 123 first street, Los Angeles CA 90012, USA
+# @copyright Copyright (C) 2018 by Rocky Shao
+# @mainurl https://www.ithacacityschools.org/
+# @supporturl https://www.ithacacityschools.org/
+# @otherurl https://www.ithacacityschools.org/
+# @license license
+# @distribution Unrestricted
+# @restrictions None
+# @ingroup modules
+#
 lower_yellow=np.array([0,0,0])
 
 higher_yellow=np.array([40,255,255])
-
-SERVER="10.6.39.2"
-from networktables import NetworkTables as nt
-def pushval(networkinstance, tablename:str, valuename, value:float):
-    table = networkinstance.getTable(tablename)
-    table.putNumber(valuename, value)
-
-def networkConnect() -> any:
-    cond = threading.Condition()
-    notified = [False]
-
-    def connectionListener(connected, info):
-        print(info, '; Connected=%s' % connected)
-        with cond:
-            notified[0] = True
-            cond.notify()
-
-    nt.initialize(server=SERVER)
-    nt.addConnectionListener(connectionListener, immediateNotify=True)
-
-    with cond:
-        print("Waiting")
-        if not notified[0]:
-            cond.wait()
-    return nt
-
-
 def maskGenerator2(img,lower_color,higher_color):
 
     #bgr math
@@ -42,8 +32,8 @@ def maskGenerator2(img,lower_color,higher_color):
     diff = cv2.subtract(g, b)
     ret, maska = cv2.threshold(diff, 28, 255, cv2.THRESH_BINARY)
     kernel1=cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
-    maska=cv2.erode(maska,kernel1,iterations=3)
-    maska=cv2.dilate(maska,kernel1,iterations=1) 
+    # maska=cv2.erode(maska,kernel1,iterations=3)
+    maska=cv2.dilate(maska,kernel1,iterations=5) 
 
     #hsv double check
     img=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
@@ -71,50 +61,26 @@ def find_center_and_draw_center_and_contour_of_target(frame,biggest_contour):
         center=((int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00'])))
         cv2.circle(frame, center, 3, (0, 0, 255), -1)
         return center
-
-class Orientation_of_Cone:
+class Orientation:
     # ###################################################################################################
     ## Constructor
     def __init__(self):
         # Instantiate a JeVois Timer to measure our processing framerate:
         self.timer = jevois.Timer("processing timer", 100, jevois.LOG_INFO)
-        self.pattern = re.compile('([0-9]*\.[0-9]+|[0-9]+) fps, ([0-9]*\.[0-9]+|[0-9]+)% CPU, ([0-9]*\.[0-9]+|[0-9]+)C,')
-        # a simple frame counter used to demonstrate sendSerial():
-        self.frame = 0
-        self.framerate_fps = "0"
-        self.CPULoad_pct = "0"
-        self.CPUTemp_C = "0"
-        self.pipelineDelay_us = "0"
-        self.redBallAngle = -1
-        self.redBallDistance = -1
-
-        self.blueBallAngle = -1
-        self.blueBallDistance = -1  
-        jevois.sendSerial("Constructor called")
-        jevois.LINFO("Constructor  logging info")
-
-
-    # ###################################################################################################
-    ## Process function with no USB output
-    def processNoUSB(self, inframe):
-        self.commonProcess(inframe=inframe)
         
     # ###################################################################################################
     ## Process function with USB output
     def process(self, inframe, outframe):
-        self.commonProcess(inframe=inframe, outframe=outframe)
 
-    def commonProcess(self, inframe, outframe = None):
-        # jevois.LINFO("inframe type is "+str(type(inframe)))
-        # Get the next camera image (may block until it is captured) and here convert it to OpenCV BGR. If you need a
-        # grayscale image, just use getCvGRAY() instead of getCvBGR(). Also supported are getCvRGB() and getCvRGBA():
         frame = inframe.getCvBGR()
+        
         # Start measuring image processing time (NOTE: does not account for input conversion time):
+            
         self.timer.start()
-        pipline_start_time = datetime.now()
-        '''
-        Vertical up is 0 degrees
-        '''
+        
+    
+
+
         mask2=maskGenerator2(frame,lower_yellow,higher_yellow)
         contours2=findContours(mask2) 
         if len(contours2) >0:
@@ -124,9 +90,9 @@ class Orientation_of_Cone:
                 center2=find_center_and_draw_center_and_contour_of_target(frame,biggest_contour2)
                 point_x2,point_y2=center2
                 #cv2.putText(frame,'{}cm,Cone'.format(distance_cm2),(point_x2,point_y2-10),0,1,(0,0,255),2)
-                approx = cv2.approxPolyDP(biggest_contour2, 150, True)
-                #  cv2.polylines(frame, [approx], True, (0, 255, 255), 2)
-                # cv2.putText(frame,str(len(approx)),(point_x2,point_y2-40),0,1,(255,0,0),2)
+                approx = cv2.approxPolyDP(biggest_contour2, 50, True)
+                cv2.polylines(frame, [approx], True, (0, 255, 0), 6)
+                cv2.putText(frame,str(len(approx)),(point_x2,point_y2-40),0,1,(255,0,0),2)
                 if len(approx)==3:
                     points_array=approx.tolist()
                     points_tuple=[]#position of the three major points
@@ -158,7 +124,7 @@ class Orientation_of_Cone:
                             min = angle
                             indexer=i
                     target_point=points_tuple[indexer]
-                    cv2.arrowedLine(frame, center2, target_point,(0,255,0), 9) 
+                    cv2.arrowedLine(frame, center2, target_point,(255,0,0), 9) 
                     x_final,y_final=target_point#point_x2,point_y2=center2
                     #difine a lower point
                     dis_center_to_target=((point_x2-x_final)**2+(point_y2-y_final)**2)**(1/2)
@@ -170,5 +136,16 @@ class Orientation_of_Cone:
                         angle_final=(-1)*angle_final
                     # cv2.arrowedLine(frame, center2, (lower_x,lower_y),(0,0,255), 9) 
                     cv2.putText(frame,str(math.degrees(angle_final)),(point_x2,point_y2-10),0,1,(255,0,0),2)
-                    nt = networkConnect()
-                    pushval(nt,'Internal' , "Orientation_of_Cone", angle_final)
+        outimg = frame
+        # Write a title:
+        cv2.putText(outimg, "JeVois Orientation", (3, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+        
+        # Write frames/s info from our timer into the edge map (NOTE: does not account for output conversion time):
+        fps = self.timer.stop()
+        height = outimg.shape[0]
+        width = outimg.shape[1]
+        cv2.putText(outimg, fps, (3, height - 6), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+        
+        # Convert our output image to video output format and send to host over USB:
+        outframe.sendCv(outimg)
+        
