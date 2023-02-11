3421cac6dba56c3f5c1c63169723107e329e20a6
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2023-02-11 16:27:45 -0500

    changed robot code

--- JevoisInternalCamera/Robotserial/src/main/java/frc/robot/Robot.java
+++ JevoisInternalCamera/Robotserial/src/main/java/frc/robot/Robot.java
@@ -4,161 +4,106 @@
 
 package frc.robot;
 
-import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
-
-import edu.wpi.first.cscore.CvSink;
-import edu.wpi.first.cscore.CvSource;
-import edu.wpi.first.cscore.MjpegServer;
-import edu.wpi.first.cscore.UsbCamera;
-import edu.wpi.first.cscore.VideoMode.PixelFormat;
-
-
-  // Color sensor testing: REMOVE
-  import edu.wpi.first.wpilibj.I2C;
-  import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-  import edu.wpi.first.wpilibj.util.Color;  
-  import com.revrobotics.ColorSensorV3;
-  import com.revrobotics.ColorMatchResult;
-  import com.revrobotics.ColorMatch;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * Uses the CameraServer class to automatically capture video from a USB webcam and send it to the
- * FRC dashboard without doing any vision processing. This is the easiest way to get camera images
- * to the dashboard. Just add this to the robotInit() method in your program.
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
  */
 public class Robot extends TimedRobot {
-  JeVoisInterface testCam;
-  private final I2C.Port i2cPort = I2C.Port.kOnboard;
-  private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-  private final ColorMatch m_colorMatcher = new ColorMatch(); 
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
   @Override
   public void robotInit() {
-    testCam = new JeVoisInterface(false);
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+    JeVoisInterface j = new JeVoisInterface();
+  }
 
-    System.out.print("Hello, World!\n");
+  /**
+   * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+   * that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+   * SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {}
 
-    //private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-    //private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-    //private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-    //private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+  /**
+   * This autonomous (along with the chooser code above) shows how to select between different
+   * autonomous modes using the dashboard. The sendable chooser code works with the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
+   * uncomment the getString line to get the auto name from the text box below the Gyro
+   *
+   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
+   * below with additional strings. If using the SendableChooser make sure to add them to the
+   * chooser code above as well.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
 
-    /*
-    m_colorMatcher.addColorMatch(kBlueTarget);
-    m_colorMatcher.addColorMatch(kGreenTarget);w
-    m_colorMatcher.addColorMatch(kRedTarget);
-    m_colorMatcher.addColorMatch(kYellowTarget); 
-    */
-    
-    
-    /*
-    // Creates UsbCamera and MjpegServer [1] and connects them
-    UsbCamera usbCamera = new UsbCamera("USB Camera 0", 0);
-    MjpegServer mjpegServer1 = new MjpegServer("serve_USB Camera 0", 1181);
-    mjpegServer1.setSource(usbCamera);
-    // Creates the CvSink and connects it to the UsbCamera
-    CvSink cvSink = new CvSink("opencv_USB Camera 0");
-    cvSink.setSource(usbCamera);
-    // Creates the CvSource and MjpegServer [2] and connects them
-    CvSource outputStream = new CvSource("Blur", PixelFormat.kMJPEG, 320, 240, 30);
-    MjpegServer mjpegServer2 = new MjpegServer("serve_Blur", 1182);
-    mjpegServer2.setSource(outputStream);
-    */
+  /** This function is called periodically during autonomous. */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
 
-
+  /** This function is called once when teleop is enabled. */
+  @Override
+  public void teleopInit() {
 
   }
 
- /**
-     * This function is called at the start of disabled
-     */
-    @Override
-    public void disabledInit() {
+  /** This function is called periodically during operator control. */
+  @Override
+  public void teleopPeriodic() {}
 
-    }
-    
-    /**
-     * This function is called periodically during disabled mode
-     */
-    @Override
-    public void disabledPeriodic() {
-        //System.out.println(testCam.getPacketRxRate_PPS());
-    }
-    
-    
-    /**
-     * This function is called at the start of autonomous
-     */
-    @Override
-    public void autonomousInit() {
+  /** This function is called once when the robot is disabled. */
+  @Override
+  public void disabledInit() {}
 
-    }
+  /** This function is called periodically when disabled. */
+  @Override
+  public void disabledPeriodic() {}
 
-    /**
-     * This function is called periodically during autonomous
-     */
-    @Override
-    public void autonomousPeriodic() {
-        
-    }
-    
-    /**
-     * This function is called at the start of teleop
-     */
-    @Override
-    public void teleopInit() {
-      System.out.println("Teleop Init.");
-      testCam.start();
-    }
+  /** This function is called once when test mode is enabled. */
+  @Override
+  public void testInit() {}
 
-    @Override
-    public void teleopExit() {
-      System.out.println("Exiting Exit.");
-      testCam.stop();
-    }
+  /** This function is called periodically during test mode. */
+  @Override
+  public void testPeriodic() {}
 
+  /** This function is called once when the robot is first started up. */
+  @Override
+  public void simulationInit() {}
 
-
-    
-    /**
-     * This function is called periodically during operator control
-     */
-    private Boolean seenCamera = false;
-
-    @Override
-    public void teleopPeriodic() {
-        //int reply = testCam.sendCmd("ping");
-        //if (reply != 5) {
-        //  System.out.println("sendCmd odd reply " + reply);
-        //}
-
-        //System.out.println("Vision Online:" + testCam.isVisionOnline());
-        //System.out.print("Target Visible: ");
-        //System.out.println(testCam.isTgtVisible());
-        //System.out.print("Target Angle: ");
-        //System.out.println(testCam.getTgtAngle_Deg());
-        //System.out.print("Target Range:");
-        //System.out.println(testCam.getTgtRange_in());
-        //System.out.print("Serial Packet RX Rate: ");
-        //System.out.println(testCam.getPacketRxRate_PPS());
-        //System.out.print("JeVois Framerate: ");
-        //System.out.println(testCam.getJeVoisFramerate_FPS());
-        //System.out.print("JeVois CPU Load: ");
-        //System.out.println(testCam.getJeVoisCpuLoad_pct());
-        //System.out.println("===============================\n\n\n");
-        
-    }
-
-    /**
-     * This function is called periodically during test mode
-     */
-    @Override
-    public void testPeriodic() {
-    } 
-
-    @Override
-    public void robotPeriodic() {
-    }
-
-  }
+  /** This function is called periodically whilst in simulation. */
+  @Override
+  public void simulationPeriodic() {}
+}
