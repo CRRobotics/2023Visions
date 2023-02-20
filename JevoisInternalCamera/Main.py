cc725f95da4f191a9ae07ca8487c47afb4a3ac64
Rocky0Shao <122405175+Rocky0Shao@users.noreply.github.com>
2023-02-20 10:55:55 -0500

    find all cone tips

--- JevoisInternalCamera/Main.py
+++ JevoisInternalCamera/Main.py
@@ -105,6 +105,29 @@ def find_center_and_draw_center_and_contour_of_target(frame,biggest_contour):
         center=((int(moments['m10']/moments['m00']), int(moments['m01']/moments['m00'])))
         cv2.circle(frame, center, 3, (0, 0, 255), -1)
         return center
+    
+'''prototype'''
+def angle_between(p1, p2, p3):
+    """Calculate the angle between three points in radians."""
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+    a = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+    b = math.sqrt((x2-x3)**2 + (y2-y3)**2)
+    c = math.sqrt((x3-x1)**2 + (y3-y1)**2)
+    return math.acos((a**2 + b**2 - c**2) / (2 * a * b))
+
+def smallest_angle_vertex(vertices):
+    """Find the vertex with the smallest angle in a polygon."""
+    angles = []
+    for i in range(len(vertices)):
+        p1 = vertices[i-1]
+        p2 = vertices[i]
+        p3 = vertices[(i+1) % len(vertices)]
+        angles.append(angle_between(p1, p2, p3))
+    min_angle = min(angles)
+    min_index = angles.index(min_angle)
+    return vertices[min_index]
 class Orientation:
     # ###################################################################################################
     ## Constructor
@@ -169,50 +192,26 @@ class Orientation:
                 approx = cv2.approxPolyDP(biggest_contour2, epsilon, True)
                 cv2.polylines(frame, [approx], True, (0, 255, 0), 6)
                 cv2.putText(frame,str(len(approx)),(point_x2,point_y2-40),0,1,(255,0,0),2)
-                if len(approx)==3:
-                    points_array=approx.tolist()
-                    points_tuple=[]#position of the three major points
-                    for i in points_array:
-                        for c in i:
-                            c=tuple(c)
-                            points_tuple.append(c)
-                    point_a=points_tuple[0]
-                    xa,ya=point_a
-                    point_b=points_tuple[1]
-                    xb,yb=point_b
-                    point_c=points_tuple[2]
-                    xc,yc=point_c
-                    ab=((yb-ya)**2+(xb-xa)**2)**(1/2)
-                    ac=((yc-ya)**2+(xc-xa)**2)**(1/2)
-                    bc=((yc-yb)**2+(xc-xb)**2)**(1/2)
-                    angle_a=math.degrees(math.acos(((ab)**2+(ac)**2-(bc)**2)/(2*(ab)*(ac))))
-                    angle_b=math.degrees(math.acos(((bc)**2+(ab)**2-(ac)**2)/(2*(bc)*(ab))))
-                    angle_c=math.degrees(math.acos(((bc)**2+(ac)**2-(ab)**2)/(2*(bc)*(ac))))
-                    lister=[]
-                    lister.append(angle_a)
-                    lister.append(angle_b)
-                    lister.append(angle_c)
-                    min=500
-                    indexer=0
-                    for i in range(3):
-                        angle=lister[i]
-                        if angle <= min:
-                            min = angle
-                            indexer=i
-                    target_point=points_tuple[indexer]
-                    cv2.arrowedLine(frame, center2, target_point,(255,0,0), 9) 
-                    x_final,y_final=target_point#point_x2,point_y2=center2
-                    #difine a lower point
-                    dis_center_to_target=((point_x2-x_final)**2+(point_y2-y_final)**2)**(1/2)
-                    lower_x=point_x2
-                    lower_y=point_y2-dis_center_to_target
-                    dis_target_to_lower=((lower_x-x_final)**2+(lower_y-y_final)**2)**(1/2)
-                    self.angle_final=math.acos(((dis_center_to_target)**2+(dis_center_to_target)**2-(dis_target_to_lower)**2)/(2*(dis_center_to_target)*(dis_center_to_target)))
-                    if x_final<point_x2:
-                        self.angle_final=(-1)*self.angle_final
-                    # cv2.arrowedLine(frame, center2, (lower_x,lower_y),(0,0,255), 9) 
-                    cv2.putText(frame,str(math.degrees(angle_final)),(point_x2,point_y2-10),0,1,(255,0,0),2)
-
+            
+                points_array=approx.tolist()
+                points_tuple=[]#position of the three major points
+                for i in points_array:
+                    for c in i:
+                        c=tuple(c)
+                        points_tuple.append(c)
+                target_point = smallest_angle_vertex(points_tuple)
+                cv2.arrowedLine(frame, center2, target_point,(255,0,0), 9) 
+                x_final,y_final=target_point#point_x2,point_y2=center2
+                #difine a lower point
+                dis_center_to_target=((point_x2-x_final)**2+(point_y2-y_final)**2)**(1/2)
+                lower_x=point_x2
+                lower_y=point_y2-dis_center_to_target
+                dis_target_to_lower=((lower_x-x_final)**2+(lower_y-y_final)**2)**(1/2)
+                self.angle_final=math.acos(((dis_center_to_target)**2+(dis_center_to_target)**2-(dis_target_to_lower)**2)/(2*(dis_center_to_target)*(dis_center_to_target)))
+                if x_final<point_x2:
+                    self.angle_final=(-1)*self.angle_final
+                # cv2.arrowedLine(frame, center2, (lower_x,lower_y),(0,0,255), 9) 
+                cv2.putText(frame,str(math.degrees(self.angle_final)),(point_x2,point_y2-10),0,1,(255,0,0),2)
     
         outimg = frame
         # Write a title:
