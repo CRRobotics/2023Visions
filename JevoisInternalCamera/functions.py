964e899c884c5b1420172072897bb71d94e249ad
Rocky0Shao <122405175+Rocky0Shao@users.noreply.github.com>
2023-02-23 12:45:50 -0500

    min area rect done

--- JevoisInternalCamera/functions.py
+++ JevoisInternalCamera/functions.py
@@ -138,27 +138,31 @@ def smallest_angle_vertex(vertices):
     min_index = angles.index(min_angle)
     return vertices[min_index]
 def polygon(center,biggest_contour,frame):
+    if len(biggest_contour) > 0:
+        '''prototype'''
+        #_________
+        box= cv2.minAreaRect(biggest_contour)
+        points = cv2.boxPoints(box)
+        asint = np.int0(points)
+        if len(asint) > 0:
+        
+            cv2.drawContours(frame,[asint],0,(0,0,255),2)
 
-    '''prototype'''
-    #_________
-    box= cv2.minAreaRect(biggest_contour)
-    points = cv2.boxPoints(box)
-    cv2.polylines(frame, [points], True, (0, 0, 255), 6)
-    c, dimensions, angle= cv2.minAreaRect(biggest_contour)
-    width,height = dimensions
-    #__________
+            c, dimensions, angle= cv2.minAreaRect(biggest_contour)
+            width,height = dimensions
+            #__________
 
-    point_x2,point_y2=center
-    epsilon = 0.07 * cv2.arcLength(biggest_contour, True)
-    approx = cv2.approxPolyDP(biggest_contour, epsilon, True)
-    cv2.polylines(frame, [approx], True, (0, 255, 0), 6)
-    cv2.putText(frame,str(len(approx)),(point_x2,point_y2-40),0,1,(255,0,0),2)
+            point_x2,point_y2=center
+            epsilon = 0.07 * cv2.arcLength(biggest_contour, True)
+            approx = cv2.approxPolyDP(biggest_contour, epsilon, True)
+            cv2.polylines(frame, [approx], True, (0, 255, 0), 6)
+            cv2.putText(frame,str(len(approx)),(point_x2,point_y2-40),0,1,(255,0,0),2)
 
-    #______
-    cv2.putText(frame,str(width/height),(point_x2,point_y2+40),0,1,(0,0,255),2)
-    #______
-    
-    return approx
+            #______
+            cv2.putText(frame,str(width/height),(point_x2,point_y2+40),0,1,(0,0,255),2)
+            #______
+            
+            return approx
 
 def find_angle(approx,frame,center):
     point_x2,point_y2 = center
