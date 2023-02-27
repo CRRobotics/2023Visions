e576667daded67fab62d4cc2ee40fe712973586d
Rocky0Shao <122405175+Rocky0Shao@users.noreply.github.com>
2023-02-27 18:13:42 -0500

    outdated changes

--- Detector/Main.py
+++ Detector/Main.py
@@ -34,16 +34,10 @@ while True:
     depth_frame = aligned_frames.get_depth_frame()
     color_frame = aligned_frames.get_color_frame()
     if not depth_frame or not color_frame: continue
-
-    
-    color_image = np.asanyarray(color_frame.get_data())
-    
-
-    
+    color_image = np.asanyarray(color_frame.get_data())  
     '''
     Cube
     '''
-
     mask1 = f.maskGenerator1(color_image)
     contours1=f.findContours(mask1)    
     #contours1=f.filter_out_contours_that_doesnot_look_like_square(contours1)
@@ -80,8 +74,8 @@ while True:
                 #test:av cords
                 dx2,dy2,dz2 = f.getCordinatesOfTarget_Cam(point_x2,point_y2, depth_frame, color_frame)
                 dx22,dy22,dz22 = f.get_average_cords(point_x2,point_y2,5,depth_frame, color_frame)
-                cv2.putText(color_image, str(int(dx2*100))+'@'+str(int(dz2*100)), (point_x2,point_y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-                cv2.putText(color_image, str(int(dx22*100))+'@'+str(int(dz22*100)), (point_x2,point_y2+20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                cv2.putText(color_image, str(int(dx2*100))+' __ '+str(int(dz2*100)), (point_x2,point_y2), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                cv2.putText(color_image, str(int(dx22*100))+' __ '+str(int(dz22*100)), (point_x2,point_y2+20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 '''
                 if dz2 != 0:
                     x2,y2,z2=f.getCordinatesOfTarget_Bot(dx2,dy2,dz2,constants.cam_mount_angle, constants.cam_height)
@@ -96,6 +90,7 @@ while True:
 
     cv2.namedWindow("color_image", cv2.WINDOW_NORMAL)
     cv2.imshow("color_image", b)
+    #set visualization frame rate
     key = cv2.waitKey(500)
     # Press esc or 'q' to close the image window
     if key & 0xFF == ord('q') or key == 27:
